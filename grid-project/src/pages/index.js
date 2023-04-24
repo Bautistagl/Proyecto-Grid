77cbe7021cd1bddb7cf1c5a10dd2497276124c45
Bautistagl <bautistagonzalezlazo@gmail.com>
2023-04-24 11:43:44 -0300

    adsa

--- grid-project/src/pages/index.js
+++ grid-project/src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
     <>
     <div className="container-homePrincipal"> 
 
-    <DynamicNavbar/>
+    {/* <DynamicNavbar/> */}
    <DynamicCarrousel/>
    <Segundo/>
    <Powered/>

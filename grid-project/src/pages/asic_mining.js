06f36c912bd69ca9b14ab024de5d465f457ccef6
Bautistagl <bautistagonzalezlazo@gmail.com>
2023-04-24 17:16:53 -0300

    dsadas

---
+++ grid-project/src/pages/asic_mining.js
@@ -0,0 +1,44 @@
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+
+import dynamic from "next/dynamic";
+import Segundo from "@/components/index/Segundo";
+import Powered from "@/components/index/Powered";
+import Faqs from "@/components/index/Faqs";
+import ContactForm from "@/components/index/ContactForm";
+import Footer from "@/components/index/Footer";
+
+
+
+const DynamicNavbar = dynamic(()=>import("../components/index/Navbar"),
+  {
+    ssr:false,
+    loading: () => <p> Im f</p>
+  }
+)
+const DynamicCarrousel = dynamic(()=>import("../components/index/Carrousel"),
+  {
+    ssr:false,
+    loading: () => <p> Im f</p>
+  }
+)
+
+export default function Home() {
+  return (
+    <>
+    <div className="container-homePrincipal"> 
+
+    <DynamicNavbar/>
+   <DynamicCarrousel/>
+   <Segundo/>
+   <Powered/>
+   <Faqs/>
+   <ContactForm/>
+   <Footer/>
+    </div>
+ 
+      
+    </>
+  )
+}

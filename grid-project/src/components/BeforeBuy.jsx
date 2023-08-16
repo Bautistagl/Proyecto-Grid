4bf5cb69f7f242a415f656d70c1f61d637f88bbb
Bautistagl <bautistagonzalezlazo@gmail.com>
2023-08-16 11:54:32 -0300

    dsad

--- grid-project/src/components/BeforeBuy.jsx
+++ grid-project/src/components/BeforeBuy.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const BeforeBuy = () => {
   return (
     <div className='contenedor-before' >
-        <h1>It seems that you don't have a project.</h1>
+        <h1>It seems that you dont have a project.</h1>
         <span> You need to complete onboarding process in order to use Grid Cloud. </span>
         <button > <Link href='/profile/hosting'> Explore setup </Link> </button>
     </div>

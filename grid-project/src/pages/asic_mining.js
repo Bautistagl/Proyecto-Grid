af4e06b2c87e8774db5d8d334bb23263489d9f31
Bautistagl <bautistagonzalezlazo@gmail.com>
2024-01-31 23:00:23 -0300

    dsadsa

--- grid-project/src/pages/asic_mining.js
+++ grid-project/src/pages/asic_mining.js
@@ -1,35 +1,35 @@
-import dynamic from "next/dynamic";
-import ContactForm from "@/components/index/ContactForm";
-import Footer from "@/components/index/Footer";
-import Banner from "@/components/landing-AsicHosting/Banner";
-import CardsAsic from "@/components/landing-AsicHosting/CardsAsic";
-import AsicDetails from "@/components/landing-AsicHosting/AsicDetails";
-import PrimerFaqs from "@/components/landing-AsicHosting/PrimerFaqs";
-import { useInView } from "react-intersection-observer";
+// import dynamic from "next/dynamic";
+// import ContactForm from "@/components/index/ContactForm";
+// import Footer from "@/components/index/Footer";
+// import Banner from "@/components/landing-AsicHosting/Banner";
+// import CardsAsic from "@/components/landing-AsicHosting/CardsAsic";
+// import AsicDetails from "@/components/landing-AsicHosting/AsicDetails";
+// import PrimerFaqs from "@/components/landing-AsicHosting/PrimerFaqs";
+// import { useInView } from "react-intersection-observer";
 
 
 
-const DynamicNavbar = dynamic(()=>import("../components/index/Navbar"),
-  {
-    ssr:false,
-    loading: () => <p> Im f</p>
-  }
-)
+// const DynamicNavbar = dynamic(()=>import("../components/index/Navbar"),
+//   {
+//     ssr:false,
+//     loading: () => <p> Im f</p>
+//   }
+// )
 
 
 export default function AsicMining() {
-  const [ref, inView] = useInView({
-    triggerOnce: true, // Animation triggers only once
-    threshold: 0.7, // Percentage of element visibility to trigger the animation
-  });
-  const fadeInStylesLeft = {
-    opacity: 1,
-    transform: 'translateX(-50px)',
-    transition: 'opacity 1s ease-in-out, transform 1s ease-in-out',
-  };
+  // const [ref, inView] = useInView({
+  //   triggerOnce: true, // Animation triggers only once
+  //   threshold: 0.7, // Percentage of element visibility to trigger the animation
+  // });
+  // const fadeInStylesLeft = {
+  //   opacity: 1,
+  //   transform: 'translateX(-50px)',
+  //   transition: 'opacity 1s ease-in-out, transform 1s ease-in-out',
+  // };
   return (
     <>
-    <div className="container-homePrincipal"> 
+    {/* <div className="container-homePrincipal"> 
 
     <DynamicNavbar/>
     <div className="banner-container">
@@ -45,7 +45,7 @@ export default function AsicMining() {
       
    <ContactForm/>
    <Footer/>
-    </div>
+    </div> */}
  
       
     </>

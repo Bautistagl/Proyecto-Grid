31940989e620cf7922a5f8566d595a36f31b4fb4
Bautistagl <bautistagonzalezlazo@gmail.com>
2024-02-28 10:05:02 -0300

    dsadsa

--- grid-project/src/components/deployBoxes/DeployMethod.jsx
+++ grid-project/src/components/deployBoxes/DeployMethod.jsx
@@ -222,12 +222,12 @@ const DeployMethod = ({ onNextStep }) => {
             <div className='no-docker'>
               <div style={{opacity:'0'}}>.</div>
               <p style={{marginTop:'30px',marginBottom:'15px'}}>No linked images found</p>
-              <p style={{marginBottom:'30px'}}> Configure linked image registries, or provide the URL of a public image (e.g. "nginx") to continue.</p>
+              <p style={{marginBottom:'30px'}}> Configure linked image registries, or provide the URL of a public image (e.g. &apos;nginx&apos;) to continue.</p>
               <div style={{opacity:'0'}}>.</div>
             </div>
             <div className="repo-git2">
             <Image alt="" src="/plus2.png" height={10} width={10} />
-            <span> Use image URL: 'prueba'</span>
+            <span> Use image URL: &apos;prueba&apos;</span>
           </div>
         <div className="repo-selected">
           <div className="circle1"> </div>
@@ -247,7 +247,7 @@ const DeployMethod = ({ onNextStep }) => {
             </div>
             <div className="repo-git2">
             <Image alt="" src="/plus2.png" height={10} width={10} />
-            <span> Use tag: 'prueba'</span>
+            <span> Use tag: &apos;prueba&apos;</span>
           </div>
             <div className="repo-selected">
           <div className="circle1"> </div>
